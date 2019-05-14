0223ac21171c5b62cfac46e5192cc2c28110301e
Steve Gehrman <sgehrman@gmail.com>
2019-05-14 10:58:00 -0700

    updated for multi directories

--- utils.js
+++ utils.js
@@ -1,30 +1,35 @@
-const { spawn } = require("child_process");
+const { spawn } = require('child_process');
 
-function run(extension) {
-  const directory = "../ore-id/web/src";
-  // const directory = "../ore-id/web"
-  // const directory = "../ore-id";
+function runOnDirectory(directory, extension) {
+  const searchPath = directory + '/**/*' + '.' + extension;
 
-  const searchPath = directory + "/**/*" + "." + extension;
-
-  const ls = spawn("node_modules/prettier/bin-prettier.js", [
-    "--config",
-    "./config.js",
-    "--write",
+  const ls = spawn('node_modules/prettier/bin-prettier.js', [
+    '--config',
+    './config.js',
+    '--write',
     searchPath
   ]);
 
-  ls.stdout.on("data", data => {
+  ls.stdout.on('data', (data) => {
     console.log(`${data}`);
   });
 
-  ls.stderr.on("data", data => {
+  ls.stderr.on('data', (data) => {
     console.log(`stderr: ${data}`);
   });
 
-  ls.on("close", code => {
+  ls.on('close', (code) => {
     console.log(`Finished files with extension: ${extension}`);
   });
+}
+
+function run(extension) {
+  // ####
+  // #### CHANGE DIRECTORY HERE
+  // ####
+
+  runOnDirectory('../oreid-portal/web', extension);
+  runOnDirectory('../oreid-service/web', extension);
 }
 
 module.exports = { run };
